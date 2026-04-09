2dfdd90d0cbb3366a3ac273dc8f54c0000fb559f
Andrea K <andreak3779@gmail.com>
2026-04-09 14:44:26 -0500

    Update project references and configurations after renaming from laughing-broccoli to MoviesAPI

--- MovieCollectionApi/obj/Debug/net9.0/MovieCollectionApi.AssemblyInfo.cs
+++ MovieCollectionApi/obj/Debug/net9.0/MovieCollectionApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieCollectionApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2438ea1ab79b4f76f98d439c685868afe183b1d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ebf8e647a38c6164658eeb64e06d0a3c29024c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieCollectionApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieCollectionApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
